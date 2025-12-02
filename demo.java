d03d294169fba309fbca019fe0550fc99da08cd3
Nara Hari <blnarahari014@gmail.com>
2025-12-02 13:24:37 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,24 @@
+
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int j;
+		int n=5;
+		for(i=1;i<=n;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				for (j=0;j<=i;j++) {
+					
+				}
+			 System.out.print("*"+" ");
+			}
+		 System.out.println();
+		}
+		
+	}
+
+}
